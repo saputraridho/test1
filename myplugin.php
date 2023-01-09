f178ef1d0f9d9cfd1878a47e935435beca50dba6
Saputra <saputraridh@gmail.com>
2023-01-09 16:41:10 +0700

    1.0

---
+++ myplugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: My Plugin
+* Plugin URl: https://www.facebook.com/putra.khecild.3/
+* Description: Ini punyaku
+* Author: PutraKun
+* Author URI: https://www.facebook.com/putra.khecild.3/
+* License: GPLv2
+* License URl: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+add_shortcode( 'shortcode_test', 'shortcode_handler_function' );
+function shortcode_handler_function(){
+    return "hello world";
+}
